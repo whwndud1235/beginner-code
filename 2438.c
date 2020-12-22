5b4385d776259a3788849b6eb41d9b450b6c985f
whwndud1235 <whwndud1235@naver.com>
2020-12-22 19:52:39 +0900

    2438.c

---
+++ 2438.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int i,j,n=0;
+    scanf("%d",&n);
+    
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++)
+        printf("*");
+        printf("\n");
+    }    
+}
