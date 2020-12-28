f07b4db66de00f51fff365713eb080ec7490a0f0
whwndud1235 <whwndud1235@naver.com>
2020-12-28 17:06:47 +0900

    koji.c

--- 2557.c/koji2.c
+++ 2557.c/koji2.c
@@ -2,5 +2,29 @@
 
 int main()
 {
-    
+    int n, i;
+    char ch;
+    int alpha[26] = {0, };
+
+    printf("input : ");
+    scanf("%d \n", &n);
+
+    int index;
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%c", &ch);
+        if (ch <= 'Z')
+            index = ch - 65;// 'A'=65 A
+        else
+            index = ch - 97;
+
+        printf("%d \n", index);
+        alpha[index]++;
+    }
+    for (i = 0; i < 26; i++)
+    {
+        if (alpha[i] > 0)
+            printf("%c = %d \n", i + 'a', alpha[i]);
+    }
 }
---
+++ 2557.c/koji3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int a =2;
+    int b =11;
+    printf("a=%d b=%d",a,b);
+}
