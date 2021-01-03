d40b23fdd9ebaf6b95d6f2e0a15434610bd54fa9
whwndud1235 <whwndud1235@naver.com>
2021-01-03 19:09:24 +0900

    3025.c*

--- 2557.c/3025.c
+++ 2557.c/3025.c
@@ -6,22 +6,22 @@ int main()
     int arr[10];
     int result =0;
 
-    int mod[10] = {0, };
+    int mod[10] = {0, };   
 
     for (i = 0; i < 10; i++)
     {
         scanf("%d", &arr[i]);
-        mod[i] = arr[i] % 42;
+        mod[i] = arr[i] % 42;  //나머지 값 대입
     }
     for (i = 0; i < 10; i++)
     {
-        int cnt = 0;
+        int cnt = 0;  //cnt 초기화
         for (j = 0; j < i; j++)
             {
             if (mod[i] == mod[j])
-                cnt++;
+                cnt++; //값이 같으면 cnt 올려서 result 증가 x
             }
-            if (cnt ==0)
+            if (cnt ==0)  //값이 다르면 result 증가
                 result++;
     }
     printf("%d ",result);
