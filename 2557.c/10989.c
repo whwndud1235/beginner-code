6c0b0e50e8af23497363add87d2474a9ac338b2d
whwndud1235 <whwndud1235@naver.com>
2021-01-09 20:15:21 +0900

    2420.c

--- 2557.c/10989.c
+++ 2557.c/10989.c
@@ -3,20 +3,19 @@
 int main()
 {
     int i, N, input,j;
-    int arr[100000] = {0, };  //메모리 초과를 피하기 위해 배열선언
+    int arr[10001] = {0, };  //메모리 초과를 피하기 위해 배열선언
 
     scanf("%d", &N);  //입력받을 정수갯수
 
-    int index; 
     for (i = 0; i < N; i++)
     {
-        scanf("%d", &input);  //입력 받고
-        arr[input]++;  //바로 인덱스 증가시키기  **포인트** <메모리 초과 피하기>
+        scanf("%d", &input);  //정수의 갯수만큼 입력을 받고
+        arr[input]++;  //입력 받자마자 바로 더해주기 **포인트** <메모리 초과 피하기>
     }
-    for (i = 0; i < 10000; i++)
+    for (i = 0; i < 10001; i++)
     {
         if (arr[i] == 0)  // 0이면 출력 x
-            continue;
+            continue;     
         for (j = 0; j < arr[i]; j++)  //index 값만큼 반복 출력 (for문에 배열쓰기 메모*)
         {
             printf("%d\n", i);
---
+++ 2557.c/2798.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()5 
+{
+    int i,j, max, N;
+    scanf("%d %d", &N, &max);
+
+    int card[N];
+    int sum = 0;
+    for (i = 0; i < N; i++)
+    {
+        scanf("%d", &card[i]);
+    }
+    
+    for(i=0;i<N;i++)
+    {
+        for(j=i+1;j<N;j++)
+        {
+            card[i]
+        }
+    }
+
+
+
+
+    for (i = 0; i < N; i++)
+    {
+        printf("%d ", card[i]);
+    }
+}
