ee0b5f9ea4017394a8aa27cf8b5693de6afdb6c2
whwndud1235 <whwndud1235@naver.com>
2021-01-16 19:57:25 +0900

    2914.py

--- 2557.c/1000.py
+++ 2557.c/1000.py
@@ -1,6 +1,1 @@
-a, b, c = input().split()  #split 함수로 공백으로 문자열 잘라 저장
-a, b, c = int(a), int(b), int(c) //int 자료형 형변환!
-print((a+b) % c)
-print(((a % c)+(b % c)) % c)
-print((a*b) % c)
-print(((a % c)*(b % c)) % c)
+# 파이썬 연습지
---
+++ 2557.c/2914.py
@@ -0,0 +1,4 @@
+s, j = input().split()
+s = int(s)
+j = int(j)
+print(s*(j-1)+1)
